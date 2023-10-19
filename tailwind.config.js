7d3113fa4eb558c16097f8959f42876cc78fb195
Nhu Thuy <tranthinhuthuyd1tp@gmail.com>
2023-10-19 14:58:25 +0700

    change directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.html"],
+  content: ["./**/*.html"],
   theme: {
     extend: {
       fontFamily: {
